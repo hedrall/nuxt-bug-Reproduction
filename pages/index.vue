f46db6bc82a4c3a60fe6692854ea0b00ebf28245
小室　雅春 <>
2019-08-12 01:37:23 +0900

    test

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+      <h1 class="title"></h1>
+    <hr>
+    <img src="~assets/logo.png" alt="">
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+  h1 {
+    background: url( '~assets/logo.png' ) no-repeat top center;
+    height: 307px;
+  }
+</style>
